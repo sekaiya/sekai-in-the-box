2b7dca7bbecf920b6079ee7d548ff8f15ee7772c
sekaiya <sekai.kobayashi@gmail.com>
2013-08-23 18:10:03 +0900

    first

---
+++ com/js/anime.js
@@ -0,0 +1,100 @@
+jQuery(document).ready(
+	function() {
+		var dfd = $.Deferred();
+		dfd.then(
+			function(){
+						return $("body").css("background-color", "#ffffff");
+			}
+		).then(
+			function(){
+				return $(".notanime").hide();
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						 $("#anime1").show();
+						dfd.resolve();
+					}, 1000);
+				});
+			}
+		).then(
+			function(){
+				return $("#anime1").animate(
+					{marginLeft: "250px",
+					 marginTop: "250px",
+					 opacity: 1}, 500, 'swing'
+				)
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						$("#anime1").animate(
+							{marginLeft: "-500px",opacity: 0}, 500
+						);
+						dfd.resolve();
+					}, 1000);
+				});
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						$("#anime2").show();
+						dfd.resolve();
+					}, 1300);
+				});
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						 $("#anime2").animate(
+							{marginLeft: "250px",
+							 marginTop: "250px",
+					 		 opacity: 1}, 500, 'swing'
+						)
+						dfd.resolve();
+					});
+				});
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						$("#anime2").hide();
+						dfd.resolve();
+					}, 3000);
+				});
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						$("body").css("background-color", "#00bfff");
+						dfd.resolve();
+					}, 800);
+				});
+			}
+		).then(
+			function(){
+				return $(".anime").remove();
+			}
+		).then(
+			function(){
+				 return jQuery.Deferred(function(dfd) {
+					setTimeout(function() {
+						$(".notanime").show(3000)
+						dfd.resolve();
+					});
+				});
+			}
+		);
+		dfd.resolve();
+
+		
+	}
+);
+
+
---
+++ com/js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+
+	$("#menus > ul > li, #menus > ul > ul > *").click(function(){
+	
+		$("#contents").hide();
+		
+		if ($("#title").length != 0) {
+			$("#title").remove();
+		}
+		$(".contents").css("background-color", "#F9FAFF");
+		
+		var pagename = $(this).attr("id");
+		$("#contents").load("inc/" + pagename + ".inc");
+		$("#contents").toggle("slow");
+	});
+	
+	$("#menus > ul > li").click(function(){
+		if(!$(this).is('.subs')){
+			$(".submenu").hide("slow");
+			return;
+		}
+		if($(this).next().is(':visible')){
+			$(".submenu").hide("slow");
+			return;
+		}
+		$(".submenu").hide();
+		$(this).next().toggle("slow");
+	});
+	
+});
+
+
