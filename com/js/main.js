48e1c68cf7d357c92565e7741779f367428d781b
sekaiya <sekai.kobayashi@gmail.com>
2013-08-25 21:50:01 +0900

    update design

--- com/js/main.js
+++ com/js/main.js
@@ -1,20 +1,25 @@
 jQuery(document).ready(function() {
 
+	var hibi_left = $(window).width();
+	var hibi_top = $(window).height();
+
+	$(".hibi").css("left", hibi_left - 250 + "px");
+	$(".hibi").css("top", hibi_top - 130 + "px");
+
 	$("#menus > ul > li, #menus > ul > ul > *").click(function(){
-		$("#contents").hide();
+	
+		if ($("#title").length != 0) { $("#title").remove(); }
 		
-		if ($("#title").length != 0) {
-			$("#title").remove();
-		}
-		$("#contents").css("background-color", "#F9FAFF");
+		if(rgbTo16($(this).css("color")) == "#ffffff") { return; }
 		
-		var pagename = $(this).attr("id");
-		$("#contents").load("inc/" + pagename + ".inc");
-		$("#contents").toggle("slow");
-		$("#contents").animate({ scrollTop: 0 }, 'fast');
+		$("#menus > ul > li, #menus > ul > ul > *").css("color", "#00617f");
+		$(this).css("color", "#ffffff");
+		
+		make_content($(this).attr("id"));
 	});
 	
 	$("#menus > ul > li").click(function(){
+		
 		if(!$(this).is('.subs')){
 			$(".submenu").hide("slow");
 			return;
@@ -29,4 +34,15 @@ jQuery(document).ready(function() {
 	
 });
 
+rgbTo16 = function(col){
+  return "#" + col.match(/\d+/g).map(function(a){return ("0" + parseInt(a).toString(16)).slice(-2)}).join("");
+}
+make_content = function(pagename){
 
+	$("#contents").hide();
+	$("#contents").css("background-color", "#ffffff");
+	
+	$("#contents").load("inc/" + pagename + ".inc");
+	$("#contents").toggle("slow");
+	$("#contents").animate({ scrollTop: 0 }, 'fast');
+}
