cb3be4336295ad16da7c955c29a6d8469aa01ce7
sekaiya <sekai.kobayashi@gmail.com>
2013-10-24 08:11:02 +0900

    update deloy bat: add message,add pause, add 'python.exe'

--- push_deploy.bat
+++ push_deploy.bat
@@ -4,7 +4,10 @@ IF %ERRORLEVEL%==1 (
 pause
 exit
 )
+echo "success comment check!!"
 FOR /F "delims=" %%a IN (mail.properties) DO (
    set MAIL=%%a
 )
-C:/google_appengine/appcfg.py --email=%MAIL% update C:/repositories/sekai-in-the-box
+python C:/google_appengine/appcfg.py --email=%MAIL% update C:/repositories/sekai-in-the-box
+
+pause
--- push_deploy.rb
+++ push_deploy.rb
@@ -5,3 +5,16 @@ File.readlines(index_file_path).each do |line|
 	next if line.index("loadingWrap").nil? &&  line.index("anime.js").nil?
 	raise "don't forget comment out" if line.index("<!--")
 end
+
+__END__
+require 'octokit'
+client = Octokit::Client.new({netrc: true})
+p client
+
+
+__END__
+
+
+https://github.com/login/oauth/
+
+
