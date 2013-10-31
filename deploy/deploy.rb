1928f8ade4d10931068d8744c5cebc7fdbd97543
sekaiya <sekai.kobayashi@gmail.com>
2013-10-31 13:02:25 +0900

    rename folder name. change gsub -> sub

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 PARENT_PATH = Pathname(File.dirname(__FILE__)).parent.to_s
 INDEX_FILE_PATH = PARENT_PATH + "/com/index.php"
 APP_YAML_PATH = PARENT_PATH + "/app.yaml"
-MAIL_PATH = PARENT_PATH + "/test/mail.properties"
+MAIL_PATH = PARENT_PATH + "/deploy/mail.properties"
 CMD = "C:/Python27/python.exe C:/google_appengine/appcfg.py --email=#{File.read(MAIL_PATH)} update C:/repositories/sekai-in-the-box"
 
 def is_comment_in?
@@ -15,7 +15,7 @@ def is_comment_in?
 end
 
 def edit_app_yaml(old_str, new_str)
-	str = File.open(APP_YAML_PATH){|f| f.read.gsub(old_str, new_str)}
+	str = File.open(APP_YAML_PATH){|f| f.read.sub(old_str, new_str)}
 	File.open(APP_YAML_PATH, "w"){|f| f.write(str)}
 end
 
