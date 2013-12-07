9ceb1a79c78a7ba9c69218eceed0a6d49ff5bd2d
sekaiya <sekai.kobayashi@gmail.com>
2013-12-07 10:59:09 +0900

    add logic: hash-url

--- com/js/main.js
+++ com/js/main.js
@@ -1,16 +1,17 @@
 jQuery(document).ready(function() {
-	$("#hibi").css("left", $(window).width() - 220 + "px");
-	$("#hibi").css("top", $(window).height() - 100 + "px");
-	var menus = $("#menus > ul > li, #menus > ul > ul > *")
+	var menus = $("#menus > ul > li, #menus > ul > ul > li")
 	menus.click(function(){
 		if($(this).css("color") == "rgb(255, 255, 255)") { return; }
 		$('html,body').animate({ scrollTop: 0 }, 'fast');
 		menus.css("color", "#00617f");
 		$(this).css("color", "#ffffff");
-		if(!$(this).is('#menus > ul > ul > *')){$(".submenu").hide("slow")};
-		if($(this).next().is('ul')){ $(this).next().toggle("slow");}
+		if(!$(this).is('#menus > ul > ul > li')){$(".submenu").hide("slow")};
+		if($(this).next().is('ul')){ $(this).next().toggle("slow")}
+		if($(this).is('#menus > ul > ul > li')){$(this).parent().show("slow")};
 		_make_content($(this).attr("id"));
 	});
+	var hurl = window.location.hash;
+	if(hurl!="" && $(hurl)[0]){$(hurl).trigger("click")};
 	$.getJSON("https://api.github.com/repos/sekaiya/sekai-in-the-box/commits?per_page=5", function(res){
 		histories = new Array();
 		for (var i=0 ; i < res.length ; i++){
@@ -19,13 +20,14 @@ jQuery(document).ready(function() {
 		};
 	});
 });
-_make_content = function(pagename){
+_make_content = function(path){
+	location.hash = path;
 	$("#contents").hide();
 	$("#contents").css("height", "");
-	if(pagename == "history") {
+	if(path == "history") {
 		_make_history_page();
 	} else {
-		$("#contents").load("inc/" + pagename + ".inc",null,function(){if($("#contents").height() < 430) { $("#contents").height(430) }});
+		$("#contents").load("inc/" + path.replace("_", "/") + ".inc",null,function(){if($("#contents").height() < 430) { $("#contents").height(430) }});
 	}
 	$("#contents").fadeIn("slow");
 }
